83db0473d59a2640f9b0149243f78318a7824ed9
Eric <emoolenbeek@gmail.com>
2025-05-07 19:48:48 -0400

    add tailwind and shadcn-svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
+    import "../app.css";
     import type { Snippet } from 'svelte';
     import type { LayoutData } from './$types';
-
+    
     let { data, children }: { data: LayoutData, children: Snippet } = $props();
 </script>
 
