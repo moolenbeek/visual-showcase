553bdd363679b622985c96df7b871ad4564e990e
Eric <emoolenbeek@gmail.com>
2025-05-08 08:04:37 -0400

    add nav

---
+++ src/lib/components/navigation/navigation.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    let { user } = $props();
+</script>
+
+<nav>
+    <!-- fix reload to show user -->
+    <p>{user?.username}</p>
+    {#if !user}
+        <a href="/login">Login</a>
+    {/if}
+</nav>
