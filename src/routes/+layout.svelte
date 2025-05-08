b8caf6adec77627d6c4b3f15c4813d7a6082aeb1
Eric <emoolenbeek@gmail.com>
2025-05-08 08:04:03 -0400

    add nav

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,12 @@
     import "../app.css";
     import type { Snippet } from 'svelte';
     import type { LayoutData } from './$types';
-    
+    import Navigation from "$lib/components/navigation/navigation.svelte";
     let { data, children }: { data: LayoutData, children: Snippet } = $props();
+    let { user } = data;
 </script>
 
 <div class="min-h-screen">
+    <Navigation user={user} />
     {@render children()}
 </div>
