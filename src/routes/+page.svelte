b19cd2ff90b2dbe5b6b05f537b985ea26878e8e0
Eric <emoolenbeek@gmail.com>
2025-05-11 16:31:54 -0400

    display user and sign out option

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang='ts'>
+	import { enhance } from '$app/forms';
+	import type { PageServerData } from './$types';
+
+	let { data }: { data: PageServerData } = $props();
+</script>
+
+<h1>Hi, {data.user.username}!</h1>
+<form method='post' action='?/logout' use:enhance>
+	<button>Sign out</button>
+</form>
