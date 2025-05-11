cb74b5a2a2744a200f22e6149bb5e8a5c7c644ff
Eric <emoolenbeek@gmail.com>
2025-05-11 17:50:06 -0400

    add gallery_item and gallery_category tables

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -13,6 +13,19 @@ export const session = pgTable('session', {
 	expiresAt: timestamp('expires_at', { withTimezone: true, mode: 'date' }).notNull()
 });
 
+export const gallery_category = pgTable('gallery_category', {
+	id: serial('id').primaryKey(),
+	name: text('name').notNull().unique(),
+	slug: text('slug').notNull().unique(),
+});
+
+export const gallery_item = pgTable('gallery_item', {
+	id: serial('id').primaryKey(),
+	categoryId: integer('category_id').references(() => gallery_category.id),
+	imageUrl: text('image_url').notNull(),
+	description: text('description').notNull(),
+});
+
 export type Session = typeof session.$inferSelect;
 
 export type User = typeof user.$inferSelect;
