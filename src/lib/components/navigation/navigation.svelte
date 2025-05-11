8d82f9eb9e5d644e4dcf0811b6011a85c84086bf
Eric <emoolenbeek@gmail.com>
2025-05-11 16:40:12 -0400

    add header

--- src/lib/components/navigation/navigation.svelte
+++ src/lib/components/navigation/navigation.svelte
@@ -3,10 +3,5 @@
 </script>
 
 <nav class="flex justify-between items-center p-4 bg-primary text-primary-foreground">
-    {#if $page.data.user}
-    <p>{$page.data.user?.username}</p>
-    {/if}
-    {#if !$page.data.user}
-        <a href="/login">Login</a>
-    {/if}
+    <h1>Website</h1>
 </nav>
