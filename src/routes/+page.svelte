98c9e650d69732940fd9692322af49263f8bbee9
Eric <emoolenbeek@gmail.com>
2025-05-11 16:38:27 -0400

    add shadcn button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,12 @@
 <script lang='ts'>
 	import { enhance } from '$app/forms';
 	import type { PageServerData } from './$types';
+	import { Button } from '$lib/components/ui/button';
 
 	let { data }: { data: PageServerData } = $props();
 </script>
 
 <h1>Hi, {data.user.username}!</h1>
 <form method='post' action='?/logout' use:enhance>
-	<button>Sign out</button>
+	<button><Button>Sign out</Button></button>
 </form>
