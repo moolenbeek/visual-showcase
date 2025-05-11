801b44bb8302def2c0e06e70168867f247899d2f
Eric <emoolenbeek@gmail.com>
2025-05-11 14:59:26 -0400

    add if statement to display user

--- src/lib/components/navigation/navigation.svelte
+++ src/lib/components/navigation/navigation.svelte
@@ -3,7 +3,9 @@
 </script>
 
 <nav class="flex justify-between items-center p-4 bg-primary text-primary-foreground">
+    {#if $page.data.user}
     <p>{$page.data.user?.username}</p>
+    {/if}
     {#if !$page.data.user}
         <a href="/login">Login</a>
     {/if}
