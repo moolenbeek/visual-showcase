5f0005458c356a7e0851fe3685cdc46885c808de
Eric <emoolenbeek@gmail.com>
2025-05-12 08:21:15 -0400

    add utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -60,3 +60,14 @@ export const flyAndScale = (
 		easing: cubicOut
 	};
 };
+
+export function slugify(text: string): string {
+	return text
+		.toString()
+		.toLowerCase()
+		.trim()
+		.replace(/\s+/g, '-')        // Replace spaces with -
+		.replace(/&/g, '-and-')      // Replace & with 'and'
+		.replace(/[^\w\-]+/g, '')    // Remove all non-word chars
+		.replace(/\-\-+/g, '-');     // Replace multiple - with single -
+}
